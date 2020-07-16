8a3e329532a3d68d676e97eaaa720f5c63eb1bdc
Lily Bartosik <lidiabartosik7@gmail.com>
2020-07-16 20:27:07 +0200

    blad

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,28 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
+
+      console.log('new Product:', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function (){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productsData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +81,13 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
+
   };
+  
+
 
   app.init();
 }
